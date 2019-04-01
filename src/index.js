f69edede7a1004f7c8a5ff741f305e13a42e20c7
Dave Torres <dave@busybusy.com>
2019-03-31 19:09:26 -0600

    Allow use of sha256 without secret

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ const Cryptr = require('cryptr')
 
 class LFCrypter {
   constructor(secret) {
-    if (!secret) throw new Error('No secret is supplied for LFCrypter')
-    this.cryptr = new Cryptr(secret)
+    this.cryptr = null
+    if (secret) {
+      this.cryptr = new Cryptr(secret)
+    }
     this.SHA256 = require("crypto-js/sha256")
   }
 
   encrypt (value) {
+    if (!this.cryptr) throw new Error('No secret is supplied for LFCrypter')
     return this.cryptr.encrypt(value)
   }
 
   decrypt (value) {
+    if (!this.cryptr) throw new Error('No secret is supplied for LFCrypter')
     return this.cryptr.decrypt(value)
   }
 
