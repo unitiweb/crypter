3e447046d27835fdc83ce983a91fb4b26538455e
Dave Torres <dave.torres@M115.local>
2019-09-12 10:51:13 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
-# lf-crypter
+# UnitiWeb's Crypter
 
-Library for encrypting and decrypting data
+This is a class that will encrypt, decrypt, and encode strings
+
+### How to use
+
+#### Install Package
+
+To install the package you would run this code
+
+```bash
+npm install unitiweb-crypter
+```
+
+#### Instantiate Class
+
+Before using you need to instantiate the class and give it a secret
+
+```js
+// A random secret key
+const secret = 'abc123def456(@%';
+
+// Instantiate the class
+const crypter = new Crypter(secret);
+```
+
+#### Using the Class
+
+Here's how you use the class to encrypt, decrypt, and encode
+
+```js
+// Setup the class
+const secret = 'abc123def456(@%'
+const crypter = new Crypter(secret)
+
+// The string
+const myString = 'This is a test'
+
+// Encrypt a string
+const myStringEncrypted = crypter.encode(myString)
+
+// Decrypt a string
+const myStringDecrypted = crypter.decrypt(myStringEncrypted)
+
+// SHA256 encode a string
+const encodedString = crypter.sha256(myString)
+```
--- src/index.js
+++ src/index.js
@@ -1,27 +1,54 @@
 const Cryptr = require('cryptr')
+const sha256 = require("crypto-js/sha256")
 
-class LFCrypter {
+class Crypter {
   constructor(secret) {
     this.cryptr = null
+
+    // A secred is only required for the encrypt and decrypt
+    // If a secret was entered we need to instantiate the Crypter class
     if (secret) {
       this.cryptr = new Cryptr(secret)
     }
-    this.SHA256 = require("crypto-js/sha256")
+
+    // The sha256 library to use
+    this.SHA256 = sha256
   }
 
+  /**
+   * Encrypt the given value using the Cryptr class
+   *
+   * @param value The value to be encrypted
+   *
+   * @return string
+   */
   encrypt (value) {
     if (!this.cryptr) throw new Error('No secret is supplied for LFCrypter')
     return this.cryptr.encrypt(value)
   }
 
+  /**
+   * Decrypt the given string that was encrypted using the encrypt method
+   *
+   * @param value The encrypted string to be decrypted
+   *
+   * @return string
+   */
   decrypt (value) {
     if (!this.cryptr) throw new Error('No secret is supplied for LFCrypter')
     return this.cryptr.decrypt(value)
   }
 
+  /**
+   * sha256 encode the given string
+   *
+   * @param value The string to encode
+   *
+   * @return string
+   */
   sha256 (value) {
     return this.SHA256(value).toString()
   }
 }
 
-module.exports = LFCrypter
+module.exports = Crypter
